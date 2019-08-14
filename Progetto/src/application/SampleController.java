1843111f26ac976fcdb21c6cf2c57093e80fcbe6
nicol <nicol@LAPTOP-RBG7QPOU>
2019-08-14 10:39:33 +0200

    fatto qualcosa ma non troppo

--- Progetto/src/application/SampleController.java
+++ Progetto/src/application/SampleController.java
@@ -1,21 +1,41 @@
 package application;
 
 import java.net.URL;
+import java.util.LinkedList;
+import java.util.List;
 import java.util.ResourceBundle;
 
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
 
 public class SampleController implements Initializable{
 	
-	@FXML private Label provaLabel;
-	@FXML private Button firstButton;
+	@FXML private Button LoginButton;
+	@FXML private Button SignUpButton;
+	@FXML private Button GuestButton;
+	
+	@FXML private PasswordField IdPasswordField;
+	@FXML private PasswordField PwPasswordField;
+	
+	@FXML private Label ErrorLabel;
+	
+	private List<User> userList=new LinkedList<User>();
 	
 	@Override
 	public void initialize(URL arg0, ResourceBundle arg1) {
-		provaLabel.setText("ciao");
 	}
 		
+	public void LoginButtonPushed() {
+		String idSup=IdPasswordField.getText();
+		String pwSup=PwPasswordField.getText();
+		
+		User userSup = new User(IdPasswordField, PwPasswordField)
+		
+		if(!userList.contains(objSup))){
+			ErrorLabel.setText("id e/o pw errati");
+		}
+	}
 }
---
+++ Progetto/src/application/User.java
@@ -0,0 +1,36 @@
+package application;
+
+public class User {
+	private String nome;
+	private String cognome;
+	private String indirizzi;
+	private String cap;
+	private String citta;
+	private String telefono;
+	private String email;
+	private String librocard;
+	private String pw="";
+	
+	
+	public User(String nome, String cognome, String indirizzi, String cap, String citta, String telefono, String email, String librocard,
+			String pw) {
+		nome=nome;
+		cognome=cognome;
+		indirizzi=indirizzi;
+		cap=cap;
+		citta=citta;
+		telefono=telefono;
+		email=email;
+		librocard=librocard;
+		pw+=pw;
+	}
+	
+	public User(String id, String pw) {
+		email=id;
+		pw+=pw;
+	}
+	
+	public compareTo(Object other) {
+		other.ins
+	}
+}
