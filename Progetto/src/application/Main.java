27dd4942c6dda5a19b2508cd2cbf0039c3558c20
NicolaGugole <nicola.gugole@gmail.com>
2019-09-18 16:51:46 +0200

    push per merge

--- Progetto/src/application/Main.java
+++ Progetto/src/application/Main.java
@@ -17,7 +17,7 @@ public class Main extends Application {
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			primaryStage.setScene(scene);
 			primaryStage.setTitle("Libreria Digitale");
-			Classifica.randomize(); //inizializzazione dei valori
+			Classifica.randomize(); //inizializzazione dei valore
 			primaryStage.show();
 		} catch(Exception e) {
 			e.printStackTrace();
